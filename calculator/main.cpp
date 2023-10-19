26e9ba02221f38863d3cb3c47135f4d75634ea60
ljx <junxun-luo@outlook.com>
2023-10-19 15:34:04 +0800

    hw03 02

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -2,7 +2,6 @@
 #include <stack>
 #include <queue>
 #include <string>
-#include <cstdlib>
 #include <cctype>
 #include "stringPreproce.h"
 
@@ -30,49 +29,53 @@ bool isnum(std::string str){
     }
 }
 
+// string中dot个数
+int countDots(const std::string& str) {  
+    int count = 0;  
+    for (char c : str) {  
+        if (c == '.') {  
+            count++;  
+        }  
+    }  
+    return count;  
+}  
+  
 
 int main(int argc, char* argv[]){
     std::queue<std::string> Postfix;//后缀表达式队列
     std::queue<std::string> infixQueue; //中缀表达式队列
     std::stack<std::string> opStack;
     infixQueue = strToQueue(strPreproce(argv[1]));
-    while (!infixQueue.empty())
-    {
-        /* code */
-        std::cout<<infixQueue.front()<<std::endl;
-        infixQueue.pop();
-    }
-
-    bool result = isnum("9.6");  
-    std::cout << std::boolalpha << result << std::endl; 
-    std::cout<<op_Priority("+")<<op_Priority("*")<<std::endl;
 
 
     std::string item;
     while (!infixQueue.empty()){
         item = infixQueue.front();
-
+        if(countDots(item) > 1){
+            err_exp();
+        }
         if(isnum(item)){
+
             Postfix.push(item);
-            infixQueue.pop();
-            continue;
         }else if(item == "("){
             opStack.push(item);
-            infixQueue.pop();
-            continue;
         }else if(item == ")"){
             while (opStack.top() != "("){
                 Postfix.push(opStack.top());
                 opStack.pop();
             }
+            // pop (
             opStack.pop();
         }else{
-            while(!opStack.empty() && op_Priority(opStack.top()) >= op_Priority(item) && opStack.top() != "("){
+            while(!opStack.empty() && (op_Priority(opStack.top()) >= op_Priority(item)) && opStack.top() != "("){
                 Postfix.push(opStack.top());
                 opStack.pop();
             }
             opStack.push(item);
         }
+        // next
+        infixQueue.pop();
+        continue;
     }
     while (!opStack.empty()){
         Postfix.push(opStack.top());
@@ -85,67 +88,6 @@ int main(int argc, char* argv[]){
     }
     std::cout<<" "<<std::endl;
     
-
-
     return 0;
     
 }
-
-// int main(int argc, char* argv[]){
-//     // 后缀表达式队列
-//     std::queue<std::string> Postfix;
-//     // 中缀表达式队列
-//     std::queue<std::string> infixQueue;
-//     // 操作符栈
-//     std::stack<std::string> opStack;
-    
-//     // 字符序列 meiyong
-//     std::string infix;
-//     infix = strPreproce(argv[1]);
-
-//     std::cout<<infix<<std::endl;
-//     infixQueue = strToQueue(infix);
-
-
-//     std::string ch = "";
-//     for(int i = 0; i < infixQueue.size(); i++){
-//         ch = infix[i];
-//         if(i == 0 && ch == "-"){
-//             Postfix.push("0");
-//         }
-//         // num and .直接push
-//         if(isnum(ch)){
-//             Postfix.push(ch);
-//             Postfix.push(" ");
-//             continue;
-//         }else if(ch == "("){
-//             opStack.push(ch);
-//         }else if(ch == ")"){
-//             while(opStack.top() != "("){
-//                 Postfix.push(opStack.top());
-//                 opStack.pop();
-//             }
-//             // pop (
-//             opStack.pop();
-//         }else{
-//             while(!opStack.empty() && op_Priority(opStack.top()) >= op_Priority(ch) && opStack.top() != "("){
-//                 Postfix.push(opStack.top());
-//                 Postfix.push(" ");
-//                 opStack.pop();
-//             }
-//             opStack.push(ch);
-//         }
-//     }
-//     while(!opStack.empty()){
-//         Postfix.push(opStack.top());
-//         opStack.pop();
-//     }
-//     while(!Postfix.empty()){
-//         std::cout<<Postfix.front();
-//         Postfix.pop();
-//     }
-//     std::cout<<" "<<std::endl;
-
-//     return 0;
-// }
-

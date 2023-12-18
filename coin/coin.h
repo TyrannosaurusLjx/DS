c294c1d60250116201eec0002f3a2e23fc9c2caa
ljx <junxun-luo@outlook.com>
2023-12-18 14:18:07 +0800

    coin finished

---
+++ coin/coin.h
@@ -0,0 +1,17 @@
+#include <vector>  
+  
+std::vector<int> coin(int n, std::vector<int> &result) {  
+    if (n >= 25) {  
+        result[3] += n / 25;  
+        return coin(n % 25, result);  
+    } else if (n >= 10) {  
+        result[2] += n / 10;  
+        return coin(n % 10, result);  
+    } else if (n >= 5) {  
+        result[1] += n / 5;  
+        return coin(n % 5, result);  
+    } else {  
+        result[0] += n;  
+        return result;  
+    }  
+}  
---
+++ coin/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include"coin.h"
+#include<numeric>
+
+int main(){
+    std::vector<int>result(4, 0);
+    std::vector<int>testN = {1,4,9,24,41,99,126};
+    std::cout<<"1 5 10 25"<<std::endl;
+    for(const auto &test: testN){
+        std::vector<int>X0(4,0);
+        result = coin(test, X0);
+
+        for (const auto &element : result) {  
+            std::cout << element << " ";  
+        }  
+
+    std::cout<< "total:"<<std::accumulate(result.begin(), result.end(), 0)<<std::endl;
+    }
+    return 0;
+}
